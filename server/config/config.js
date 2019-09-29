12f175818d076fceb5fca9b025c57c7cf4f85c22
Danielle Martin <32286415+dannie1010@users.noreply.github.com>
2019-09-29 19:34:10 -0400

    Add files via upload

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    db: {
+      uri: 'mongodb+srv://dannie:dandan@bootcamp2-qh2zn.mongodb.net/test?retryWrites=true&w=majority'//place the URI of your mongo database here.
+    }, 
+    openCage: {
+      key: 'def9abd3b7344930b87ff7341de87e83' //place your openCage public key here - Sign-up for a free key https://opencagedata.com/
+    },
+    port: 8080
+  };
